8735bd7eec1e118481f2be03e56d2dc0c328e44b
kumarstack55 <kumarstack55@gmail.com>
2020-02-08 04:46:44 +0000

    delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source .venv/bin/activate
-export FLASK_APP=app.py
-export FLASK_ENV=development
-#export FLASK_ENV=production
-export FLASK_RUN_PORT=80
-flask run --host 0.0.0.0

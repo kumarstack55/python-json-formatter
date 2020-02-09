6782efa2f0204844124954b1e13da54f2e15243e
kumarstack55 <kumarstack55@gmail.com>
2020-02-09 02:00:54 +0000

    Refactor

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
-# coding: utf-8
-
+import argparse
 import flask
-import html
 import jinja2
 import json
-import argparse
+import wtforms
 
 
-SORT_KEYS_DEFAULT = False
-INDENT_DEFAULT = 2
-JSON_SAMPLE = '''
+JSON_SAMPLE = '''\
 { "menu": { "id": "file", "value": "File", "popup": {
 "menuitem": [ {"value": "New", "onclick": "CreateNewDoc()"},
 {"value": "Open", "onclick": "OpenDoc()"}, {"value": "Close",
@@ -21,79 +17,57 @@ JSON_SAMPLE = '''
 app = flask.Flask(__name__)
 
 
-def form_to_bool(raw_value: str) -> bool:
-    return raw_value == 't'
+class Form(wtforms.Form):
+    enable_sort_keys = wtforms.BooleanField(
+            "enable_sort_keys", default=False)
+    enable_indent = wtforms.BooleanField(
+            "enable_indent", default="checked")
+    indent_width = wtforms.SelectField(
+            "indent_width", choices=[(2, "2"), (4, "4"), (8, "8")],
+            default=2, coerce=int)
+    enable_json_only = wtforms.BooleanField(
+            "enable_json_only", default=False)
+    input_json = wtforms.TextAreaField(
+            "input_json", default=JSON_SAMPLE)
+    output_json = wtforms.TextAreaField("output_json")
 
 
-def form_to_int(raw_value: str) -> bool:
-    if raw_value == 'None':
-        return None
-    return int(raw_value)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if flask.request.method == "POST":
+        form = Form(flask.request.form)
+    else:
+        form = Form()
 
-
-def get_indent_items(indent) -> list:
-    indent_items = []
-    for v in ['None', 2, 4]:
-        item = dict()
-
-        item['value'] = v
-
-        item['selected'] = ''
-        if v == indent:
-            item['selected'] = ' selected'
-
-        item['text'] = v
-        if v == 'None':
-            item['text'] = 'Compact'
-
-        indent_items.append(item)
-    return indent_items
-
-
-def get_body(method: str, form: dict) -> str:
-    ctx = dict()
-
-    json_text_raw = JSON_SAMPLE
-    if method == 'POST':
-        json_text_raw = form['in']
-
-    ctx['sort_keys'] = SORT_KEYS_DEFAULT
-    if method == 'POST':
-        ctx['sort_keys'] = \
-                form_to_bool(form.get('sort_keys', 'f'))
-
-    ctx['indent'] = INDENT_DEFAULT
-    if method == 'POST':
-        ctx['indent'] = form_to_int(form['indent'])
-
-    ctx['in'] = html.escape(json_text_raw)
+    indent = None
+    if form.enable_indent.data:
+        indent = form.indent_width.data
 
     try:
-        data = json.loads(json_text_raw)
-        ctx['out'] = html.escape(
-                json.dumps(
-                    data, sort_keys=ctx.get('sort_keys'),
-                    indent=ctx.get('indent')))
+        form.output_json.data = json.dumps(
+                json.loads(form.input_json.data),
+                sort_keys=form.enable_sort_keys.data,
+                indent=indent)
+        if form.enable_json_only.data:
+            return flask.Response(
+                form.output_json.data,
+                mimetype="application/json; charset=utf-8")
     except json.decoder.JSONDecodeError as e:
-        ctx['out'] = html.escape(str(e))
+        if form.enable_json_only.data:
+            abort(400)
+        form.output_json.data = str(e)
 
-    ctx['indent_items'] = get_indent_items(ctx['indent'])
-
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
-    template = env.get_template('index.html.j2')
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader("."))
+    template = env.get_template("index.html.j2")
+    ctx = dict(form=form)
     return template.render(ctx)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return get_body(flask.request.method, flask.request.form)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', default='0.0.0.0')
-    parser.add_argument('--port', '-p', type=int, default=5000)
-    parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument("--host", default="0.0.0.0")
+    parser.add_argument("--port", "-p", type=int, default=5000)
+    parser.add_argument("--debug", action="store_true", default=False)
     args = parser.parse_args()
 
     app.run(host=args.host, port=args.port, debug=args.debug)
